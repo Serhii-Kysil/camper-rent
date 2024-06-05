ef7fd7329032c67f7bfa8bacf6a3b503d622dbcb
Serhii-Kysil <serik.kisel000@gmail.com>
2024-06-05 12:41:45 +0300

    Add Welcome Page + Navigation

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+import css from "./WelcomePage.module.css";
+import { FaArrowRight } from "react-icons/fa";
+
 export default function WelcomePage() {
   return (
-    <div>
-      <h1>WELCOMEPAGE</h1>
+    <div className={css.cont}>
+      <h1 className={css.title}>Welcome to Our Camper Rental Service</h1>
+      <p className={css.desc}>
+        We offer a variety of campers for rent across Ukraine...
+      </p>
+      <Link to="/catalog" className={css.link}>
+        Got to the catalog <FaArrowRight />
+      </Link>
     </div>
   );
 }
