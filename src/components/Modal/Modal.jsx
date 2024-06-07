dba55ecfb8e77bde374b653929f0274a937fe7dd
Serhii-Kysil <serik.kisel000@gmail.com>
2024-06-08 00:01:40 +0300

    add form

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -61,9 +61,13 @@ export const Modal = ({ camper, onClose, reviewCount, city, country }) => {
       .max(50, "Maximum 50 characters"),
     email: Yup.string()
       .required("Email is required")
-      .email("Invalid email format"),
+      .email("Invalid email format")
+      .matches(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Invalid email format"
+      ),
     date: Yup.date()
-      .required("Date is required")
+      // .required("Date is required")
       .default(() => new Date()),
     textarea: Yup.string(),
   });
@@ -270,6 +274,12 @@ export const Modal = ({ camper, onClose, reviewCount, city, country }) => {
                 </div>
               )}
               <div className={css.formContainer}>
+                <div className={css.formDesc}>
+                  <h3 className={css.formTitle}>Book your campervan now</h3>
+                  <p className={css.formDesc}>
+                    Stay connected! We are always ready to help you.
+                  </p>
+                </div>
                 <Formik
                   initialValues={{
                     name: "",
@@ -280,62 +290,73 @@ export const Modal = ({ camper, onClose, reviewCount, city, country }) => {
                   validationSchema={validationSchema}
                 >
                   <Form autoComplete="off" className={css.form}>
-                    <div className={css.formGroup}>
-                      <Field
-                        placeholder={"Name"}
-                        className={css.formFiled}
-                        type="text"
-                        name="name"
-                        id={nameField}
-                      />
-                      <ErrorMessage
-                        className={css.error}
-                        name="name"
-                        component="span"
-                      />
+                    <div className={css.fields}>
+                      <div className={css.formGroup}>
+                        <Field
+                          placeholder={"Name"}
+                          className={`${css.formFiled} ${css.nameFiled}`}
+                          type="text"
+                          name="name"
+                          id={nameField}
+                        />
+                        <ErrorMessage
+                          className={css.error}
+                          name="name"
+                          component="span"
+                        />
+                      </div>
+                      <div className={css.formGroup}>
+                        <Field
+                          placeholder={"Email"}
+                          className={css.formFiled}
+                          type="text"
+                          name="email"
+                          id={emailField}
+                        />
+                        <ErrorMessage
+                          className={css.error}
+                          name="email"
+                          component="span"
+                        />
+                      </div>
+                      <div
+                        className={css.formGroup}
+                        style={{ position: "relative" }}
+                      >
+                        <Field
+                          placeholder={"Booking date"}
+                          className={`${css.formFiled} ${css.dateFiled}`}
+                          type="text"
+                          name="date"
+                          id={dateField}
+                        />
+                        <CiCalendar className={css.calendarIcon} />
+                        <ErrorMessage
+                          className={css.error}
+                          name="date"
+                          component="span"
+                        />
+                      </div>
+                      <div className={css.formGroup}>
+                        <Field
+                          as="textarea"
+                          placeholder={"Comment"}
+                          className={`${css.formFiled} ${css.textareaField}`}
+                          type="text"
+                          name="textarea"
+                          id={commentField}
+                        />
+                        <ErrorMessage
+                          className={css.error}
+                          name="textarea"
+                          component="span"
+                        />
+                      </div>
                     </div>
-                    <div className={css.formGroup}>
-                      <Field
-                        placeholder={"Email"}
-                        className={css.formFiled}
-                        type="text"
-                        name="email"
-                        id={emailField}
-                      />
-                      <ErrorMessage
-                        className={css.error}
-                        name="email"
-                        component="span"
-                      />
-                    </div>
-                    <div className={css.formGroup}>
-                      <Field
-                        placeholder={"Boocking date"}
-                        className={css.formFiled}
-                        type="text"
-                        name="date"
-                        id={dateField}
-                      />
-                      <ErrorMessage
-                        className={css.error}
-                        name="date"
-                        component="span"
-                      />
-                      <CiCalendar className={css.calendarIcon} />
-                    </div>
-                    <div className={css.formGroup}>
-                      <Field
-                        placeholder={"Comment"}
-                        className={css.formFiled}
-                        type="text"
-                        name="textarea"
-                        id={commentField}
-                      />
-                      <ErrorMessage
-                        className={css.error}
-                        name="textarea"
-                        component="span"
-                      />
+                    <div className={css.btnGroup}>
+                      <button className={css.btn} type="submit">
+                        Send
+                      </button>
                     </div>
                   </Form>
                 </Formik>
