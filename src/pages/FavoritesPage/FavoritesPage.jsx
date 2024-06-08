c0b7c56d5bbd3b6b71aefb51c6701b6af0fe7d74
Serhii-Kysil <serik.kisel000@gmail.com>
2024-06-08 13:22:54 +0300

    Add Favorite page

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,7 +1,23 @@
+import css from "./FavoritesPage.module.css";
+
+import { useSelector } from "react-redux";
+import { selectFav } from "../../redux/Campers/selector";
+import { AdItem } from "../../components/AdItem/AdItem";
+
 export default function FavoritesPage() {
+  const favorites = useSelector(selectFav);
   return (
-    <div>
-      <h1>FAVORITEPAGE</h1>
+    <div className={css.pageContainer}>
+      <h1 className={css.pageTitle}>My Favorite Campers</h1>
+      {favorites.length > 0 ? (
+        <ul className={css.favList}>
+          {favorites.map((camper) => (
+            <AdItem key={camper._id} camper={camper} />
+          ))}
+        </ul>
+      ) : (
+        <p className={css.emptyMessage}>You have no favorite campers.</p>
+      )}
     </div>
   );
 }
