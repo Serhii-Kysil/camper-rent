6d44495b977215c54b68ec18f3aeb20de87bdef9
Serhii-Kysil <serik.kisel000@gmail.com>
2024-06-08 13:38:17 +0300

    Add cursor

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -10,9 +10,31 @@ import { LuShowerHead } from "react-icons/lu";
 
 const Filter = () => {
   const [inputValue, setInputValue] = useState("");
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [selectedRadio, setSelectedRadio] = useState("");
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
+  };
+
+  const handleCheckboxClick = (id) => {
+    setSelectedItems((prevSelectedItems) =>
+      prevSelectedItems.includes(id)
+        ? prevSelectedItems.filter((item) => item !== id)
+        : [...prevSelectedItems, id]
+    );
+  };
+
+  const isChecked = (id) => selectedItems.includes(id);
+
+  const handleRadioClick = (id) => {
+    setSelectedRadio(id);
+  };
+
+  const isRadioChecked = (id) => selectedRadio === id;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -36,128 +58,124 @@ const Filter = () => {
       </div>
       <div className={css.formContainer}>
         <p className={css.titles}>Filters</p>
-        <form className={css.form}>
+        <form className={css.form} onSubmit={handleSubmit}>
           <div className={css.equipment}>
             <h3>Vehicle equipment</h3>
             <hr className={css.line} />
             <ul className={css.checkboxCont}>
-              <li className={css.checkboxItem}>
-                <input
-                  type="checkbox"
-                  id="ac"
-                  name="check"
-                  className={css.checkInput}
-                />
-                <label htmlFor="ac" className={css.checkboxLabel}>
-                  <FaWind className={css.checkboxSvg} />
-                  <span className={css.span}>AC</span>
-                </label>
-              </li>
-              <li className={css.checkboxItem}>
-                <input
-                  type="checkbox"
-                  id="automatic"
-                  name="check"
-                  className={css.checkInput}
-                />
-                <label htmlFor="automatic" className={css.checkboxLabel}>
-                  <TbAutomaticGearbox className={css.checkboxSvg} />
-                  <span className={css.span}>Automatic</span>
-                </label>
-              </li>
-              <li className={css.checkboxItem}>
-                <input
-                  type="checkbox"
-                  id="kitchen"
-                  name="check"
-                  className={css.checkInput}
-                />
-                <label htmlFor="kitchen" className={css.checkboxLabel}>
-                  <TbToolsKitchen2 className={css.checkboxSvg} />
-                  <span className={css.span}>Kitchen</span>
-                </label>
-              </li>
-              <li className={css.checkboxItem}>
-                <input
-                  type="checkbox"
-                  id="tv"
-                  name="check"
-                  className={css.checkInput}
-                />
-                <label htmlFor="tv" className={css.checkboxLabel}>
-                  <PiTelevisionSimple className={css.checkboxSvg} />
-                  <span className={css.span}>TV</span>
-                </label>
-              </li>
-              <li className={css.checkboxItem}>
-                <input
-                  type="checkbox"
-                  id="shower"
-                  name="check"
-                  className={css.checkInput}
-                />
-                <label htmlFor="shower" className={css.checkboxLabel}>
-                  <LuShowerHead className={css.checkboxSvg} />
-                  <span className={css.span}>Shower/WC</span>
-                </label>
-              </li>
+              {[
+                {
+                  id: "ac",
+                  label: "AC",
+                  icon: <FaWind className={css.checkboxSvg} />,
+                },
+                {
+                  id: "automatic",
+                  label: "Automatic",
+                  icon: <TbAutomaticGearbox className={css.checkboxSvg} />,
+                },
+                {
+                  id: "kitchen",
+                  label: "Kitchen",
+                  icon: <TbToolsKitchen2 className={css.checkboxSvg} />,
+                },
+                {
+                  id: "tv",
+                  label: "TV",
+                  icon: <PiTelevisionSimple className={css.checkboxSvg} />,
+                },
+                {
+                  id: "shower",
+                  label: "Shower/WC",
+                  icon: <LuShowerHead className={css.checkboxSvg} />,
+                },
+              ].map(({ id, label, icon }) => (
+                <li
+                  key={id}
+                  className={`${css.checkboxItem} ${
+                    isChecked(id) ? css.checked : ""
+                  }`}
+                  onClick={() => handleCheckboxClick(id)}
+                >
+                  <input
+                    type="checkbox"
+                    id={id}
+                    name="check"
+                    className={css.checkInput}
+                    checked={isChecked(id)}
+                    onChange={() => handleCheckboxClick(id)}
+                  />
+                  <label htmlFor={id} className={css.checkboxLabel}>
+                    {icon}
+                    <span className={css.span}>{label}</span>
+                  </label>
+                </li>
+              ))}
             </ul>
           </div>
           <div className={css.typeCont}>
             <h3>Vehicle type</h3>
             <hr className={css.line} />
             <ul className={css.radioCont}>
-              <li className={css.radioItem}>
-                <input
-                  type="radio"
-                  id="van"
-                  name="radio"
-                  className={css.radioInput}
-                />
-                <label htmlFor="van" className={css.radioLabel}>
-                  <svg className={css.radioSvg}>
-                    <use
-                      href="/symbol-defs.svg#icon-Van"
-                      className={css.radioIcon}
-                    ></use>
-                  </svg>
-                  <span className={css.span}>Van</span>
-                </label>
-              </li>
-              <li className={css.radioItem}>
-                <input
-                  type="radio"
-                  id="fully"
-                  name="radio"
-                  className={css.radioInput}
-                />
-                <label htmlFor="fully" className={css.radioLabel}>
-                  <svg className={css.radioSvg}>
-                    <use
-                      href="/symbol-defs.svg#icon-FullyIntegrated"
-                      className={css.radioIcon}
-                    ></use>
-                  </svg>
-                  <span className={css.span}>Fully Integrated</span>
-                </label>
-              </li>
-              <li className={css.radioItem}>
-                <input
-                  type="radio"
-                  id="alcove"
-                  name="radio"
-                  className={css.radioInput}
-                />
-                <label htmlFor="alcove" className={css.radioLabel}>
-                  <svg className={css.radioSvg}>
-                    <use
-                      href="/symbol-defs.svg#icon-AlcoveCamper"
-                      className={css.radioIcon}
-                    ></use>
-                  </svg>
-                  <span className={css.span}>Alcone</span>
-                </label>
-              </li>
+              {[
+                {
+                  id: "van",
+                  label: "Van",
+                  icon: (
+                    <svg className={css.radioSvg}>
+                      <use
+                        href="/symbol-defs.svg#icon-Van"
+                        className={css.radioIcon}
+                      ></use>
+                    </svg>
+                  ),
+                },
+                {
+                  id: "fully",
+                  label: "Fully Integrated",
+                  icon: (
+                    <svg className={css.radioSvg}>
+                      <use
+                        href="/symbol-defs.svg#icon-FullyIntegrated"
+                        className={css.radioIcon}
+                      ></use>
+                    </svg>
+                  ),
+                },
+                {
+                  id: "alcove",
+                  label: "Alcove",
+                  icon: (
+                    <svg className={css.radioSvg}>
+                      <use
+                        href="/symbol-defs.svg#icon-AlcoveCamper"
+                        className={css.radioIcon}
+                      ></use>
+                    </svg>
+                  ),
+                },
+              ].map(({ id, label, icon }) => (
+                <li
+                  key={id}
+                  className={`${css.radioItem} ${
+                    isRadioChecked(id) ? css.checked : ""
+                  }`}
+                  onClick={() => handleRadioClick(id)}
+                >
+                  <input
+                    type="radio"
+                    id={id}
+                    name="radio"
+                    className={css.radioInput}
+                    checked={isRadioChecked(id)}
+                    onChange={() => handleRadioClick(id)}
+                  />
+                  <label htmlFor={id} className={css.radioLabel}>
+                    {icon}
+                    <span className={css.span}>{label}</span>
+                  </label>
+                </li>
+              ))}
             </ul>
           </div>
           <button type="submit" className={css.btn}>
