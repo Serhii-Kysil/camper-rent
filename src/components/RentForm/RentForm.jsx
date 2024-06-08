ca98aaf0b9822e97223cb8ab000a2719bf70e8f3
Serhii-Kysil <serik.kisel000@gmail.com>
2024-06-08 12:04:28 +0300

    Add datepicker

--- src/components/RentForm/RentForm.jsx
+++ src/components/RentForm/RentForm.jsx
@@ -1,15 +1,21 @@
 import css from "./RentForm.module.css";
+import { useState } from "react";
 
 import { CiCalendar } from "react-icons/ci";
 import { Field, Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useId } from "react";
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 export const RentForm = () => {
   const nameField = useId();
   const emailField = useId();
   const dateField = useId();
   const commentField = useId();
+  const [startDate, setStartDate] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const validationSchema = Yup.object().shape({
     name: Yup.string()
@@ -29,6 +35,12 @@ export const RentForm = () => {
     textarea: Yup.string(),
   });
 
+  const handleDateChange = (date, setFieldValue) => {
+    setStartDate(date);
+    setFieldValue("date", date);
+    setIsOpen(false);
+  };
+
   return (
     <div className={css.formContainer}>
       <div className={css.formDesc}>
@@ -46,73 +58,90 @@ export const RentForm = () => {
         }}
         validationSchema={validationSchema}
       >
-        <Form autoComplete="off" className={css.form}>
-          <div className={css.fields}>
-            <div className={css.formGroup}>
-              <Field
-                placeholder={"Name"}
-                className={`${css.formFiled} ${css.nameFiled}`}
-                type="text"
-                name="name"
-                id={nameField}
-              />
-              <ErrorMessage
-                className={css.error}
-                name="name"
-                component="span"
-              />
+        {({ setFieldValue }) => (
+          <Form autoComplete="off" className={css.form}>
+            <div className={css.fields}>
+              <div className={css.formGroup}>
+                <Field
+                  placeholder={"Name"}
+                  className={`${css.formFiled} ${css.nameFiled}`}
+                  type="text"
+                  name="name"
+                  id={nameField}
+                />
+                <ErrorMessage
+                  className={css.error}
+                  name="name"
+                  component="span"
+                />
+              </div>
+              <div className={css.formGroup}>
+                <Field
+                  placeholder={"Email"}
+                  className={css.formFiled}
+                  type="text"
+                  name="email"
+                  id={emailField}
+                />
+                <ErrorMessage
+                  className={css.error}
+                  name="email"
+                  component="span"
+                />
+              </div>
+              <div className={css.formGroup} style={{ position: "relative" }}>
+                <Field
+                  placeholder={"Booking date"}
+                  className={`${css.formFiled} ${css.dateFiled}`}
+                  type="text"
+                  name="date"
+                  id={dateField}
+                  value={startDate ? startDate.toLocaleDateString() : ""}
+                  readOnly
+                />
+                <CiCalendar
+                  className={css.calendarIcon}
+                  onClick={() => setIsOpen(!isOpen)}
+                />
+                {isOpen && (
+                  <div className={css.datePickerWrapper}>
+                    <DatePicker
+                      selected={startDate}
+                      onChange={(date) => handleDateChange(date, setFieldValue)}
+                      inline
+                      onClickOutside={() => setIsOpen(false)}
+                    />
+                  </div>
+                )}
+                <ErrorMessage
+                  className={css.error}
+                  name="date"
+                  component="span"
+                />
+              </div>
+              <div className={css.formGroup}>
+                <Field
+                  as="textarea"
+                  placeholder={"Comment"}
+                  className={`${css.formFiled} ${css.textareaField}`}
+                  type="text"
+                  name="textarea"
+                  id={commentField}
+                />
+                <ErrorMessage
+                  className={css.error}
+                  name="textarea"
+                  component="span"
+                />
+              </div>
             </div>
-            <div className={css.formGroup}>
-              <Field
-                placeholder={"Email"}
-                className={css.formFiled}
-                type="text"
-                name="email"
-                id={emailField}
-              />
-              <ErrorMessage
-                className={css.error}
-                name="email"
-                component="span"
-              />
+            <div className={css.btnGroup}>
+              <button className={css.btn} type="submit">
+                Send
+              </button>
             </div>
-            <div className={css.formGroup} style={{ position: "relative" }}>
-              <Field
-                placeholder={"Booking date"}
-                className={`${css.formFiled} ${css.dateFiled}`}
-                type="text"
-                name="date"
-                id={dateField}
-              />
-              <CiCalendar className={css.calendarIcon} />
-              <ErrorMessage
-                className={css.error}
-                name="date"
-                component="span"
-              />
-            </div>
-            <div className={css.formGroup}>
-              <Field
-                as="textarea"
-                placeholder={"Comment"}
-                className={`${css.formFiled} ${css.textareaField}`}
-                type="text"
-                name="textarea"
-                id={commentField}
-              />
-              <ErrorMessage
-                className={css.error}
-                name="textarea"
-                component="span"
-              />
-            </div>
-          </div>
-          <div className={css.btnGroup}>
-            <button className={css.btn} type="submit">
-              Send
-            </button>
-          </div>
-        </Form>
+          </Form>
+        )}
       </Formik>
     </div>
   );
